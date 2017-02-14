a549dfe931f2f5b8796c28d9783ec2ef287853b0
Zeqli <l.caecar@gmail.com>
2017-02-13 20:36:21 -0600

    Add solution generator to repo

---
+++ sed/SolutionGeneratorFileOnly.sh
@@ -0,0 +1,10 @@
+#!/bin/bash          
+echo Generating...
+file="raw.txt"
+
+# 1. Remove whitespaces and capitalize every first letter (\u - to uppercase next character)
+# 2. Remove inline comment modifier '\\' and insert 'Problem' to the head and append '.java' file ext.
+newfile=$(sed -n -e '1 {s/[[:space:]]*\b\(.\)/\u\1/g
+                        s@/*[[:space:]]*\(.*\)@Problem\1\.java@p}' $file)
+echo $newfile
+#cp $file $newfile
--- sed/SolutionGeneratorWithGit.sh
+++ sed/SolutionGeneratorWithGit.sh
@@ -4,11 +4,9 @@ file="raw.txt"
 
 # 1. Remove whitespaces and capitalize every first letter (\u - to uppercase next character)
 # 2. Remove inline comment modifier '\\' and insert 'Problem' to the head and append '.java' file ext.
-# 3. Print only first line to output stream
-newfile=$(sed -n -e '1{s/[[:space:]]*\b\(.\)/\u\1/g
-                  s/^\/\/\(.*\)$/Problem\1\.java/
-				  p}' $file)
+newfile=$(sed -n -e '1 {s/[[:space:]]*\b\(.\)/\u\1/g
+                        s@/*[[:space:]]*\(.*\)@Problem\1\.java@p}' $file)
 echo $newfile
 cp $file $newfile
 git add $newfile
-git commit -m \"$newfile\"
+git commit -m \"Add $newfile\"
