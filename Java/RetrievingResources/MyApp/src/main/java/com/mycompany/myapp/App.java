bd16ef598325fc5a3c6c549ff153047e030459e6
Zeqli <l.caecar@gmail.com>
2017-04-07 21:50:47 -0500

    Project done

--- Java/RetrievingResources/MyApp/src/main/java/com/mycompany/myapp/App.java
+++ Java/RetrievingResources/MyApp/src/main/java/com/mycompany/myapp/App.java
@@ -13,38 +13,5 @@ import java.net.URL;
  */
 public class App {
     public static void main(String[] args){
-        App app = new App();
-        app.loadFile();
-    }
-    
-    public void loadFile(){
-        URL url;
-        // False. /com/mycompany/myapp/file3.txt does not exist
-        url = getClass().getResource("file3.txt");
-        System.out.println("URL1 = " + url);
-        
-        // True. classes/file3.txt
-        url = getClass().getResource("/file3.txt");
-        System.out.println("URL2 = " + url);
-        
-        // True. classes/file3.txt
-        url = getClass().getResource("../../../file3.txt");
-        System.out.println("URL3 = " + url);
-        
-        // True
-        url = getClass().getClassLoader().getResource("file3.txt");
-        System.out.println("URL4 = " + url);
-        
-        // False. Classloader.getResource('filename') filename cannot start with leading '/'
-        url = getClass().getClassLoader().getResource("/file3.txt");
-        System.out.println("URL5 = " + url);
-        
-        // True
-        url = getClass().getClassLoader().getResource("./file3.txt");
-        System.out.println("URL6 = " + url);        
-        
-        // True
-        url = Thread.currentThread().getContextClassLoader().getResource("file3.txt");
-        System.out.println("URL7 = " + url);        
     }
 }
---
+++ Java/RetrievingResources/MyApp/src/test/java/com/mycompany/myapp/AppTest.java
@@ -0,0 +1,111 @@
+package com.mycompany.myapp;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLDecoder;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class AppTest {
+    
+    private static final Logger logger = LogManager.getRootLogger();
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+        logger.info("---------- classpath ----------");
+        ClassLoader classloader = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader)classloader).getURLs();
+        for(URL url: urls){
+            logger.info(url.getPath().substring(1));
+        }
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testResourcesRetrieval() {
+        URL url1 = getClass().getClassLoader().getResource("file3.txt");
+        URL url2 = getClass().getClassLoader().getResource("./file3.txt");
+        URL url3 = Thread.currentThread().getContextClassLoader().getResource("file3.txt");
+        URL url4 = getClass().getClassLoader().getResource("/file3.txt"); 
+        
+        assertNotNull(url1);
+        assertNotNull(url2);    
+        assertNotNull(url3);
+        assertNull(url4); // Classloader.getResource('filename') filename cannot start with leading '/'
+    }
+    
+    @Test
+    public void testClassGetResource(){
+        // Classpath: target/classes/
+        URL url1 = getClass().getResource("/file3.txt");
+        URL url2 = getClass().getResource("../../../file3.txt");
+        URL url3 = getClass().getResource("file3.txt");
+        
+        
+        assertNotNull(url1);
+        assertNotNull(url2);  
+        assertNull(url3);       // /com/mycompany/myapp/file3.txt does not exist
+    }
+    
+    @Test
+    public void testGetResourcePath(){
+        logger.info("---------- URI and URL ----------");
+        ClassLoader classLoader = getClass().getClassLoader();
+        
+        URI uri = null;
+        URL url = null;
+        try {
+            uri = classLoader.getResource("file3.txt").toURI();
+            url = classLoader.getResource("file3.txt");
+            logger.info(uri.getScheme());
+            
+            logger.info("---------- Encoded Paths ----------");
+            logger.info(url.getFile());   // encoded path /C:/User/...
+            logger.info(url.getPath());   // same as above
+            logger.info(uri.getRawSchemeSpecificPart());  // URL without file:
+            
+            logger.info("---------- Dncoded Paths ----------");
+            logger.info(uri.getPath());    // decoded /C:/Users/zeqing/...
+
+            logger.info("---------- URI Paths ----------");
+            logger.info(url.toExternalForm());
+            logger.info(url.toString());
+            logger.info(uri.toURL().toString());                     // file:/C:/Users/zeqing/....
+            logger.info(uri.toString());        // same as toURL
+
+            logger.info("---------- URLDecoder.decode(<encoded-string>, \"UTF-8\") ----------");
+            String s = URLDecoder.decode(url.getPath().substring(1), "UTF-8");
+            logger.info(s);
+            
+        } catch (URISyntaxException | MalformedURLException | UnsupportedEncodingException e) { }
+        
+    }
+
+}
